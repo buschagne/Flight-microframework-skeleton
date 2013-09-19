0d145822b959fdfaca72ef3408b2ea7009fb0778
wil <wil.labuschagne@bewan.be>
2013-09-19 13:16:53 +0200

    twitter bootstrap

--- views/index/index.php
+++ views/index/index.php
@@ -1,1 +1,5 @@
-Hello world!
+<div class="container">
+    <div class="panel">
+        <p>Hello world!</p>
+    </div>
+</div>
--- views/layouts/default.php
+++ views/layouts/default.php
@@ -4,6 +4,7 @@
     <meta charset=utf-8 />
     <title><?php echo $layoutTitle ?></title>
     <link rel="stylesheet" type="text/css" media="screen" href="css/master.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="twitter-bootstrap/bootstrap.css" />
     <!--[if IE]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->

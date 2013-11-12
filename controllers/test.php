a32bd73ca7623f2de7d2f164428509afc0b00aa2
wil <wil.labuschagne@bewan.be>
2013-11-12 14:56:56 +0100

    get data

--- controllers/test.php
+++ controllers/test.php
@@ -14,13 +14,25 @@ class Test extends BaseController {
      */
     public static function index()
     {
+        //$data = self::data();
+        self::view()->set('layoutTitle', 'SF - Test');
+        self::view()->set('page_title', 'Test');
+        self::view()->set('num', rand(0, 10));
+        //self::view()->set('bean', $data);
+        self::view()->set('bean', self::data());
+
+        self::render();
+    }
+    
+    function data(){
+        
         R::setup('mysql:host=localhost;
         dbname=redbeanphptest', 'root', 'user');
 
         //Ready. Now insert a bean!
         $bean = R::dispense('leaflet');
-        $bean->title = 'Rant & Rave';
-        $bean->description = 'Rant & Rave is a new entry on the ...';
+        $bean->title = '3rd Rant & Rave';
+        $bean->description = '3rd Rant & Rave is a new entry on the ...';
 
         //Store the bean
         $id = R::store($bean);
@@ -31,13 +43,8 @@ class Test extends BaseController {
         //Display the title
         //echo $leaflet->title;
         //$leaflet->title;
-
-        self::view()->set('layoutTitle', 'SF - Test');
-        self::view()->set('page_title', 'Test');
-        self::view()->set('num', rand(0, 10));
-        self::view()->set('bean', $leaflet->title);
-
-        self::render();
+        
+        return $leaflet->title;
     }
 
 }

e26bafff253c2b0546bc09946907e31f6551d6fe
wil <wil.labuschagne@bewan.be>
2013-11-12 12:15:07 +0100

    panel style change

--- views/test/index.php
+++ views/test/index.php
@@ -1,4 +1,4 @@
-<div class="panel panel-default">
+<div class="panel panel-primary">
     <div class="panel-heading"><h3><?php echo $page_title ?></h3></div>
     <div class="panel-body">
         <p>Some random number: <?php echo $num ?></p>
